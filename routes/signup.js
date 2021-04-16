daa01800be733aa825af8cd20b71bf631af37c2f
RufailG <girgis.rufail@iisgalvanimi.edu.it>
2021-04-16 09:01:08 +0000

    secure question required

--- routes/signup.js
+++ routes/signup.js
@@ -6,10 +6,12 @@ var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://TripNav:tripnavigation@Primo.jweu5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-/* GET users listing. */
-router.get('/:nick/:pass', function (req, res){
+/* GET users listing.*/
+router.get('/:nick/:pass/:quest/:answr', function (req, res){
     nick = req.params.nick;
     pass = req.params.pass;
+    quest = req.params.quest;
+    answr = req.params.answr;
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("TripNav");
@@ -18,18 +20,18 @@ router.get('/:nick/:pass', function (req, res){
             this.result = results;
           if (result.length === 0)
           {
-            var myobj = { "nickname": nick , "password":pass };
+            var myobj = { "nickname": nick , "password":pass , "question":quest , "answer":answr};
             dbo.collection("users").insertOne(myobj, function(err, res) {
             if (err) throw err;
             this.result = results;
             result = "user added";
             });
-            var reg = {"SingUped" : false}
+            var reg = {"SingUped" : true, "nickname":nick}
             result = reg;
             res.send(result);
           }else
           {
-            var reg = {"SingUped" : true}
+            var reg = {"SingUped" : false}
             result = reg;
             res.send(result);
           }
